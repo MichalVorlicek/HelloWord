8e913168f3f3435f87a8e0904cdbf15b8453bcf2
michal.vorlicek <michal.vorlicek@W10120.sps-cl.local>
2018-03-26 12:23:03 +0200

    cimmit

---
+++ src/nazdar/světe/NazdarSvěte.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nazdar.světe;
+
+/**
+ *
+ * @author michal.vorlicek
+ */
+public class NazdarSvěte {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("nazdar bazar!");
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + " ");
+            
+        }
+    }
+    
+}
